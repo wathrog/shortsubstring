901b4e72677b5aae673d38112990816ee3fa7caf
Francesco Salbaroli <francesco.salbaroli@gmail.com>
2012-06-20 12:43:56 +0200

    Initial commit

---
+++ src/main/java/org/francesco/shortsubstring/App.java
@@ -0,0 +1,13 @@
+package org.francesco.shortsubstring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
